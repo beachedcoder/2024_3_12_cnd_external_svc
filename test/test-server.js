7290be833d1c34e753b5d5caf24508696220f556
nathan <nathan@fjmertz.com>
2024-03-15 15:03:58 +0000

    fixing changes

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("Demo</h1>"));
+        chai.assert.isTrue(res.text.includes("Svc</h1>"));
         return done();
       });
 
